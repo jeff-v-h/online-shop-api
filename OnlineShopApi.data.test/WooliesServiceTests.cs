6a5ee54b7123f5f731ef0b130b2859e785f29939
Jeffrey Huang <jeffvh@outlook.com>
2020-02-08 18:21:21 +1100

    consider not found responses in service

--- OnlineShopApi.data.test/WooliesServiceTests.cs
+++ OnlineShopApi.data.test/WooliesServiceTests.cs
@@ -5,6 +5,7 @@ using OnlineShopApi.data.Models;
 using OnlineShopApi.data.Repositories;
 using RichardSzalay.MockHttp;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using Xunit;
 
@@ -12,7 +13,7 @@ namespace OnlineShopApi.data.test
 {
     public class WooliesServiceTests
     {
-        private readonly WooliesService _service;
+        private readonly WooliesService _service; // use this private variable for for OK status responses only
         private readonly Mock<IConfiguration> _mockConfig;
         private readonly MockHttpMessageHandler _mockHttp;
         private const string Host = "http://test.com";
@@ -27,11 +28,12 @@ namespace OnlineShopApi.data.test
             _mockConfig = new Mock<IConfiguration>();
             _mockConfig.Setup(a => a.GetSection(It.Is<string>(s => s == "WooliesService"))).Returns(mockConfSection.Object);
             _mockHttp = new MockHttpMessageHandler();
-            var httpClient = ConfigureHttpClient(_mockHttp);
 
+            var httpClient = ConfigureHttpClient(_mockHttp);
             _service = new WooliesService(_mockConfig.Object, httpClient);
         }
-
+        
+        // Only for OK Status responses
         private HttpClient ConfigureHttpClient(MockHttpMessageHandler mockHttp)
         {
             var history = JsonConvert.SerializeObject(new List<ShopperHistory>());
@@ -66,12 +68,42 @@ namespace OnlineShopApi.data.test
         }
 
         [Fact]
+        public async void GetShopperHistory_ReturnNull_WhenNotFound()
+        {
+            var mockHttp = new MockHttpMessageHandler();
+
+            mockHttp.When($"{Host}/api/resource/shopperHistory?token={Token}").Respond(HttpStatusCode.NotFound);
+
+            var httpClient = new HttpClient(mockHttp);
+            var service = new WooliesService(_mockConfig.Object, httpClient);
+
+            var result = await service.GetShopperHistoryAsync();
+
+            Assert.Null(result);
+        }
+
+        [Fact]
         public async void GetProducts_ReturnsListOfProducts()
         {
             var result = await _service.GetProductsAsync();
 
             Assert.NotNull(result);
             Assert.IsType<List<Product>>(result);
+        }
+
+        [Fact]
+        public async void GetProducts_ReturnNull_WhenNotFound()
+        {
+            var mockHttp = new MockHttpMessageHandler();
+
+            mockHttp.When($"{Host}/api/resource/products?token={Token}").Respond(HttpStatusCode.NotFound);
+
+            var httpClient = new HttpClient(mockHttp);
+            var service = new WooliesService(_mockConfig.Object, httpClient);
+
+            var result = await service.GetProductsAsync();
+
+            Assert.Null(result);
         }
 
         [Fact]
